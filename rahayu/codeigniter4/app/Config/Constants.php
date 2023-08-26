886511547408768d9b8e7d83ff4256ac154fa857
Semara Incorporated <semaraincorporated@gmail.com>
2023-08-26 22:53:40 +0800

    [HACK] Update Constants.php

--- rahayu/codeigniter4/app/Config/Constants.php
+++ rahayu/codeigniter4/app/Config/Constants.php
@@ -18,7 +18,7 @@ defined('APP_NAMESPACE') || define('APP_NAMESPACE', 'App');
 //BaseURL Dynamic
 $server_name = "100.64.90.79" //!empty($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost:8080';
 
-$base_url = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $server_name . '/';
+$base_url = (!empty($_SERVER['HTTPS']) ? 'http' : 'http') . '://' . $server_name . '/';
 defined('BASE') || define('BASE', $base_url);
 
 /*
