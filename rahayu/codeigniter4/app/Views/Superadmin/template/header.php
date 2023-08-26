3ecf0e5d21e9542b470ab9dacdf43e291c4dc1a5
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-08-26 21:18:27 +0800

    update addon config

--- rahayu/codeigniter4/app/Views/Superadmin/template/header.php
+++ rahayu/codeigniter4/app/Views/Superadmin/template/header.php
@@ -11,7 +11,7 @@
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/1ae4387474.js" crossorigin="anonymous"></script>
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?= base_url('assets/addon/css/adminlte.min.css') ?>">
+    <link rel="stylesheet" href="<?= site_url('assets/addon/css/adminlte.min.css') ?>">
     <!-- SweetAlertV2 -->
     <script src="
 https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js
@@ -21,10 +21,6 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css
 " rel="stylesheet">
     <!-- ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
-    <!-- data table -->
-    <link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>">
     <!-- addon css content wrapper -->
     <!-- ajax -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
