a33216d4b60099883fff552787abb25e9c6b6b5c
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-08-26 15:15:26 +0800

    Pembaruan sistem codeigniter4

---
+++ rahayu/codeigniter4/app/Views/Superadmin/template/sidebar.php
@@ -0,0 +1,134 @@
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-dark">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <a class="d-block text-white">Halo, <?= session()->get('nama') ?></a>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li id="realtime-clock" class="d-block text-white"></li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4" style="position: fixed; top:0;">
+            <!-- Brand Logo -->
+            <a class="brand-link mb-2">
+                <img src="<?= base_url('img/support.png') ?>" alt="HelpDesk" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">HelpDesk</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                        <!-- Dashboard menu item -->
+                        <li class="nav-item">
+                            <a href="<?= base_url('admin/dashboard') ?>" class="nav-link">
+                                <i class="nav-icon fas fa-house"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Laporan menu item -->
+                        <li class="nav-item has-treeview" style="margin-top: 10px;">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-file fa-lg"></i>
+                                <p>
+                                    Laporan
+                                    <i class="fas fa-angle-left right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="<?= base_url('admin/laporan') ?>" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Semua Laporan</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="<?= base_url('admin/laporanp') ?>" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Laporan Diproses</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="<?= base_url('admin/laporans') ?>" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Laporan Selesai</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="<?= base_url('admin/laporand') ?>" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Laporan Ditolak</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- Pengguna menu item -->
+                        <li class="nav-item">
+                            <a href="<?= base_url('admin/pengguna') ?>" class="nav-link">
+                                <i class="nav-icon fas fa-users"></i>
+                                <p>
+                                    Pengguna
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Keluar menu item -->
+                        <li class="nav-item">
+                            <a href="<?= base_url('admin/myinfo') ?>" class="nav-link">
+                                <i class="nav-icon fas fa-user"></i>
+                                <p>
+                                    Profile Saya
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Keluar menu item -->
+                        <li class="nav-item">
+                            <?php if (session()->get('isLoggedIn')) : ?>
+                                <a href="#" class="nav-link" onclick="logoutConfirmation()">
+                                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                                <?php endif; ?>
+                                <p>
+                                    Keluar
+                                </p>
+                                </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- SweetAlert Logout 1 -->
+        <script>
+            function logoutConfirmation() {
+                Swal.fire({
+                    title: 'Sudah selesai?',
+                    text: "Anda akan keluar dari halaman ini.",
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    cancelButtonText: 'Batal',
+                    confirmButtonText: 'Keluar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = "<?= base_url('/logout') ?>";
+                    }
+                })
+            }
+        </script>
+
+</body>
