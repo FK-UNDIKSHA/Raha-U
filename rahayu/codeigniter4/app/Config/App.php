8107a03b8f8e9b18f18ad449a24cd25b9b00d695
Semara Incorporated <semaraincorporated@gmail.com>
2023-08-26 21:42:17 +0800

    Set Root URI App.php

--- rahayu/codeigniter4/app/Config/App.php
+++ rahayu/codeigniter4/app/Config/App.php
@@ -24,7 +24,9 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+    $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+    public $baseURL = $root; //'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
