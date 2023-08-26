5fde318a235e5fb34cdc99ae23eee9c2d0ef8658
Semara Incorporated <semaraincorporated@gmail.com>
2023-08-26 20:30:45 +0800

    Fix Namespace in Dashboard.php

--- rahayu/codeigniter4/app/Controllers/Superadmin/Dashboard.php
+++ rahayu/codeigniter4/app/Controllers/Superadmin/Dashboard.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Controllers;
+//namespace App\Controllers;
+namespace App\Controllers\Superadmin;
+use CodeIgniter\Controller;
 
 class Dashboard extends BaseController
 {
     public function index()
     {
-        return view('Superadmin/dashboard');
+        return view('\App\Views\Superadmin\dashboard.php');
     }
 }
