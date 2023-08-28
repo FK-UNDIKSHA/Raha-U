058bb9c4455ec96cbddad7b7b6d05af95249a213
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-08-28 17:33:12 +0800

    Update Dashboard controllers

--- rahayu/codeigniter4/app/Controllers/Superadmin/Dashboard.php
+++ rahayu/codeigniter4/app/Controllers/Superadmin/Dashboard.php
@@ -9,6 +9,6 @@ class Dashboard extends BaseController
     public function index()
     {
         return view('\App\Views\Superadmin\dashboard.php',
-                   ['title' => "Superadmin Dashboard",]);
+                   ['title' => "SA | Dashboard",]);
     }
 }
