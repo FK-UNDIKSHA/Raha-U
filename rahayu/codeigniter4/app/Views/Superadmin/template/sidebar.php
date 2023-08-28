08280ade127b04011ba63074f83d9b0630170b84
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-08-28 17:02:48 +0800

    Update sidebar code

--- rahayu/codeigniter4/app/Views/Superadmin/template/sidebar.php
+++ rahayu/codeigniter4/app/Views/Superadmin/template/sidebar.php
@@ -21,7 +21,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4" style="position: fixed; top:0;">
             <!-- Brand Logo -->
             <a class="brand-link mb-2">
-                <img src="<?= base_url('img/support.png') ?>" alt="HelpDesk" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="<?= base_url('img/support.png') ?>" alt="RAHA-U" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">RAHA-U</span>
             </a>
 
