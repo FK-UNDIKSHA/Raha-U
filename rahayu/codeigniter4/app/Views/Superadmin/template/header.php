f74702161a824b8cb7279b58c25f6b7910c03551
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-09-02 12:08:59 +0800

    update superadmin code

--- rahayu/codeigniter4/app/Views/Superadmin/template/header.php
+++ rahayu/codeigniter4/app/Views/Superadmin/template/header.php
@@ -18,6 +18,11 @@
     <!-- ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
     <!-- addon css content wrapper -->
+    <!-- data table -->
+    <link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>">
+
     <!-- ajax -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <style>
@@ -29,9 +34,6 @@
             color: black;
         }
     </style>
-    <!-- DataTables -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
 </head>
 <?= view('Superadmin/template/sidebar') ?>
