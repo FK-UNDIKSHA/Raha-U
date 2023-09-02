306f7f7cfce390d4d2a2b66426bf0e1abb98d728
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-09-02 11:35:10 +0800

    Update pengguna for Superadmin

--- rahayu/codeigniter4/app/Views/Superadmin/pengguna.php
+++ rahayu/codeigniter4/app/Views/Superadmin/pengguna.php
@@ -8,6 +8,9 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
+                    <h1 class="m-0">Data Pengguna</h1>
+                </div>
+                <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li id="realtime-clock" class="breadcrumb-item"></li>
                     </ol>
