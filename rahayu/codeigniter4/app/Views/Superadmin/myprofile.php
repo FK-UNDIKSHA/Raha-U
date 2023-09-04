924707e1e79a1af91052cffb15e27de81e4cffeb
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-09-04 18:54:40 +0800

    Update myprofile code on superadmin

--- rahayu/codeigniter4/app/Views/Superadmin/myprofile.php
+++ rahayu/codeigniter4/app/Views/Superadmin/myprofile.php
@@ -1,6 +1,5 @@
-<title><?= $title ?></title>
 <?= view('superadmin/template/header') ?>
-
+<title><?= $title ?></title>
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
