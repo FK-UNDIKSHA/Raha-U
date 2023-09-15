ca9a71606186c7206a37c1466c9f8ce7eff59421
anjastslt23 <100746173+anjastslt23@users.noreply.github.com>
2023-09-15 14:38:02 +0800

    Update for dokter

---
+++ rahayu/codeigniter4/app/Views/dokter/kons_selesai.php
@@ -0,0 +1,68 @@
+<!-- Header -->
+<?= view('Superadmin/template/header') ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Konsultasi Selesai</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li id="realtime-clock" class="breadcrumb-item"></li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Tabel Superadmin -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header" style="background-color: #2060C2;">
+                            <h3 class="card-title" style="color: white;">Informasi</h3>
+                            <!-- <button type="button" class="btn btn-success float-right" onclick="window.location='<?php echo site_url('superadmin/addsa') ?>'">
+                                <i class="fas fa-user-plus"></i>
+                                Add Data
+                            </button> -->
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="superadmintableforSA" class="table table-bordered table-striped display">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">No</th>
+                                        <th class="text-center">Nama</th>
+                                        <th class="text-center">Email</th>
+                                        <th class="text-center">No. WhatsApp</th>
+                                        <th class="text-center">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- no data -->
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- login sukses -->
+<?= view('Superadmin/template/footer') ?>
