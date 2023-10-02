2b23e7719d4bfca74e3bad23823f737aa2a43603
Rama Ngurah Putera Pinatih <112929305+Gramaidon@users.noreply.github.com>
2023-10-03 00:00:28 +0800

    Upload LandingPages
    fix pages= home.php ,  about.php , login.php
    akses LandingPages: [php spark serve] browser "http://localhost:8080"

---
+++ rahayu-LandingPages/app/Views/login.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+    <title>KLINIK CONVERT</title>
+
+    <link rel="stylesheet" href="../assets/css/maicons.css">
+
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    
+    <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+    
+    <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+    
+    <link rel="stylesheet" href="../assets/css/theme.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+<body>
+
+<html lang="en">
+<head>
+  
+  <style>
+    .bg-image-vertical {
+      position: relative;
+      overflow: hidden;
+      background-repeat: no-repeat;
+      background-position: right center;
+      background-size: auto 100%;
+    }
+    @media (min-width: 1025px) {
+      .h-custom-2 {
+        height: 100%;
+      }
+    }
+  </style>
+</head>
+<body>
+  <section class="vh-100">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-sm-6 text-black d-flex justify-content-center align-items-center">
+              <form style="width: 23rem;">
+                  <h3 class="fw-normal mb-3 pb-3 mt-5" style="letter-spacing: 1px;">Log in</h3>
+                  <div class="form-outline mb-4">
+                      <input type="email" id="form2Example18" class="form-control form-control-lg" />
+                      <label class="form-label" for="form2Example18">Email address</label>
+                  </div>
+                  <div class="form-outline mb-4">
+                      <input type="password" id="form2Example28" class="form-control form-control-lg" />
+                      <label class="form-label" for="form2Example28">Password</label>
+                  </div>
+                  <div class="pt-1 mb-4">
+                      <button class="btn btn-info btn-lg btn-block" type="button">Login</button>
+                  </div>
+                  <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+                  <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+              </form>
+          </div>
+        <div class="col-sm-6 px-0 d-none d-sm-block">
+          <img src="../assets/img/doctors/doctor_1.jpg" alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+        </div>
+      </div>
+    </div>
+  </section>
+    
+<script src="../assetes/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+
+</body>
+</html>
